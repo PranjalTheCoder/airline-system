9aa43e82e1c4265e2b25e783fc2e0f662e6893df
Pranjal Tamrakar <154547923+PranjalTheCoder@users.noreply.github.com>
2026-04-05 00:58:19 +0530

    Delete backend/auth-service directory

--- backend/auth-service/src/main/java/com/airline/auth_service/repository/PermissionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.airline.auth_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.airline.auth_service.entity.Permission;
-
-public interface PermissionRepository extends JpaRepository<Permission, Long> {
-}
