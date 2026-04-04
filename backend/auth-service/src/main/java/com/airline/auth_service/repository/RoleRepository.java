606e74558b7d10396cf039c24b195a90ae5c6dae
Pranjal Tamrakar <pranjaltamrakar0794@gmail.com>
2026-04-05 02:43:19 +0530

    fix: auth-service conflict

--- backend/auth-service/src/main/java/com/airline/auth_service/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.airline.auth_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.airline.auth_service.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
